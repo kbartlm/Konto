e20ae56cb26f542eab089d977e683681864f4378
Kai Bartlmae <kai.bartlmae@gmail.com>
2015-05-23 18:15:08 +0800

    add

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+
+
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    ]
---
+++ blog/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.db import models
+from jpype import *
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def post_list(request):
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+
+
+
+    return render(request, 'blog/post_list.html',{'posts':posts})
+
+@login_required
+def post_detail(request, pk):
+    post=get_object_or_404(Post, pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form=PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
+
+@login_required
+def post_edit(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form=PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form=PostForm(instance=post)
+    return render(request,'blog/post_edit.html',{'form':form})
