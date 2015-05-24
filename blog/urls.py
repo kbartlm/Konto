31ef040c8058ac8b98e777c0190a27cd4cb725d7
Kai BArtlmae <kai.bartlmae@gmail.com>
2015-05-24 22:22:14 +0800

    add

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^post/(?P<pk>[0-9]+)/remove/$', views.post_remove, name='post_remove'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     ]
